1e585844c8267ad9fbb8b56e779c08109ead7ee2
Stephen Li <stephen.li305@gmail.com>
2018-12-12 16:00:17 -0800

    snapshot works

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -9,16 +9,45 @@ const snap = document.querySelector('.snap');
 function getVideo(){
   navigator.mediaDevices.getUserMedia({video:true, audio:false})
     .then(localMediaStream => {
-      console.log(localMediaStream);
+      // console.log(localMediaStream);
       video.srcObject = localMediaStream;
       video.play();
     })
     .catch(err => {
       console.error("oh no");
     })
+  // console.log("hi");
+}
 
 
-  console.log("hi");
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+  console.log(width, height);
+
+  return  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    const pixels = ctx.getImageData(0,0, width, height);
+    console.log(pixels);
+    debugger
+
+  }, 16);
+}
+
+function takePhoto(){
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download','tiffiiii');
+  link.innerHTML = `<img src="${data}" alt="TiffSwaggy"/> `
+  strip.insertBefore(link, strip.firstChild);
+
 }
 
 getVideo()
+video.addEventListener('canplay',paintToCanvas)
